b3af7d5d14d446b5e5e96ad7f31515ae9cf77d7c
caovn <520qianjiawen@sina.com>
2025-06-02 09:16:53 +0800

    damao

--- server/sources/zhihu.ts
+++ server/sources/zhihu.ts
@@ -21,7 +21,7 @@ interface Res {
 
 export default defineSource({
   zhihu: async () => {
-    const url = "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=20&desktop=true"
+    const url = "https://www.zhihu.com/api/v3/feed/topstory/hot-list-web?limit=20&desktop=true"
     const res: Res = await myFetch(url)
     return res.data
       .map((k) => {
