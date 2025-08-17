d700d6bb01f3b206d0a24aa480662398ba14e236
520qianjiawen <520qianjiawen@sina.com>
2025-08-17 15:15:12 +0800

    x

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,11 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
+      <a href="https://github.com/ASG/newsnow/blob/main/LICENSE" target="_blank">MIT LICENSE</a>
       <span>
-<<<<<<< HEAD
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-=======
         <span>NewsNow © 2025 By </span>
-<<<<<<< HEAD
-        <a href={Author.url} target="_blank">
-=======
         <a href="https://github.com/ASG/newsnow" target="_blank">
->>>>>>> 64c8cbfeca5b584f7f28f625893ae578128db310
           ASG
->>>>>>> 0bb1e277bac8ac6b7a74066bd3acb452804ce0f2
         </a>
       </span>
     </>
