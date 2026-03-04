cbbb70288ac2823ffaeff9e23d223f196f6e9130
520qianjiawen <520qianjiawen@sina.com>
2026-03-04 14:54:53 +0800

    fix: replace dead reuters rss feed with google news reuters feed

--- server/sources/world.ts
+++ server/sources/world.ts
@@ -1,5 +1,5 @@
 export default defineSource({
-  reuters: defineRSSSource("https://feeds.reuters.com/reuters/topNews"),
+  reuters: defineRSSSource("https://news.google.com/rss/search?q=when:24h+allinurl:reuters.com&hl=en-US&gl=US&ceid=US:en"),
   bbc: defineRSSSource("http://feeds.bbci.co.uk/news/world/rss.xml"),
   apnews: defineRSSHubSource("/apnews/topics/apf-topnews"),
   aljazeera: defineRSSSource("https://www.aljazeera.com/xml/rss/all.xml"),
